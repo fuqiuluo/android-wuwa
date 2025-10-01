d122f6230f2f3091464f495915d73707aaf8a680
ella8192 <wuwa@wuwa233.icu>
2025-10-02 01:03:01 +0800

    fix build

--- wuwa_proc.h
+++ wuwa_proc.h
@@ -1,8 +1,12 @@
 #ifndef ANDROID_WUWA_WUWA_PROC_H
 #define ANDROID_WUWA_WUWA_PROC_H
 
-#include <signal.h>
-#include <sched.h>
+#include <linux/mm.h>
+#include <linux/mm_types.h>
+#include <linux/types.h>
+#include <linux/version.h>
+#include "wuwa_common.h"
+#include <linux/sched.h>
 
 #define PF_INVISIBLE 0x10000000
 
